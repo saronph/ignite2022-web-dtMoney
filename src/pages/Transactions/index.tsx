ffa3731a23cf06364efe610e22522800605dc9c7
Saron Medeiros Philippi <saron.ph@gmail.com>
2022-08-29 18:04:10 -0300

    added table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { SearchForm } from './components/SearchForm';
 
 import * as S from './styles';
 
@@ -9,6 +10,35 @@ export function Transactions() {
     <div>
       <Header />
       <Summary />
+
+      <S.TransactionsContainer>
+        <SearchForm />
+
+        <S.TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <S.PriceHighLight variant='income'>
+                  R$ 12.000,00
+                </S.PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Aluguel</td>
+              <td>
+                <S.PriceHighLight variant='outcome'>
+                  - R$ 2.000,00
+                </S.PriceHighLight>
+              </td>
+              <td>Aluguel</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </S.TransactionsTable>
+      </S.TransactionsContainer>
     </div>
   );
 }
--- src/pages/Transactions/styles.tsx
+++ src/pages/Transactions/styles.tsx
@@ -1,11 +1,43 @@
 import styled from 'styled-components';
 
-export const Container = styled.main`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
 `;
 
-export const Title = styled.label`
-  font-size: 16px;
+export const TransactionsTable = styled.table`
+  width: 100%;
+
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+
+  margin-top: 1.5rem;
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${(props) => props.theme['gray-700']};
+
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`;
+
+interface PriceHighLightProps {
+  variant: 'income' | 'outcome';
+}
+
+export const PriceHighLight = styled.span<PriceHighLightProps>`
+  color: ${(props) =>
+    props.variant === 'income'
+      ? props.theme['gray-300']
+      : props.theme['red-300']};
 `;
